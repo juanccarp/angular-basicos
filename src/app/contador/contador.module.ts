5d65ec59395aec8366511db5fd2d2217668eeea2
Juan Carlos Goytia <goytia_juan@redlink.com.ar>
2021-10-08 16:54:59 -0300

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from "@angular/core";
+import { ContadorComponent } from './contador/contador.component';
+
+@NgModule({
+  declarations: [
+    ContadorComponent
+  ],
+  exports: [
+    ContadorComponent
+  ]
+})
+
+export class ContadorModule {}
